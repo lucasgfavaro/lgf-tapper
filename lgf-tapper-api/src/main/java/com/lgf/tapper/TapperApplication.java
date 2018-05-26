0a89a1b4f600c5edc790eaa6c715c7714ea4fea8
Lucas Favaro <lucasgracianofavaro@gmail.com>
2018-05-26 20:30:22 -0300

    Initial commit

---
+++ lgf-tapper-api/src/main/java/com/lgf/tapper/TapperApplication.java
@@ -0,0 +1,12 @@
+package com.lgf.tapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapperApplication.class, args);
+	}
+}
